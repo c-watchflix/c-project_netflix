53d5150c88f5a9e95e7a34ede7c96a00c6872a28
Nathan <carriounathan742@gmail.com>
2025-12-03 16:06:59 +0100

    add : TestAdoMusique

--- watchflix/Repositories/AdoMusique.cs
+++ watchflix/Repositories/AdoMusique.cs
@@ -1,53 +1,131 @@
 using System;
-using System.Data.SqlClient;   
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using watchflix.Models;
 
 namespace watchflix.Repositories;
 
 public class AdoMusique : Ado
 {
-    public static void GetAllMusique()
+    public static List<Musique> GetAllMusique()
     {
         open();
-        SqlCommand cmd = new SqlCommand();
-        cmd.Connection = connexion;
-        cmd.CommandText = "SELECT * FROM Musique";
-        cmd.ExecuteNonQuery();
-        close();
+        List<Musique> musiques = new List<Musique>();
+        try
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connexion;
+            cmd.CommandText = "SELECT * FROM Musique";
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                musiques.Add(new Musique(
+                    reader.GetInt32(0), // id_musique
+                    reader.GetString(1), // titre_musique
+                    reader.GetTimeSpan(2), // duree_musique
+                    reader.GetString(3), // album
+                    reader.GetString(4)  // couverture
+                ));
+            }
+            reader.Close();
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Erreur SQL : {ex.Message}");
+        }
+        finally
+        {
+            close();
+        }
+        return musiques;
     }
 
-    public static void GetOneById(int id_musique)
+    public static Musique GetOneById(int id_musique)
     {
         open();
-        SqlCommand cmd = new SqlCommand();
-        cmd.Connection = connexion;
-        cmd.CommandText = "SELECT * FROM Musique WHERE id_musique = @id_musique";
-        cmd.Parameters.AddWithValue("@id_musique", id_musique);
-        cmd.ExecuteNonQuery();
-        close();
+        Musique musique = null;
+        try
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connexion;
+            cmd.CommandText = "SELECT * FROM Musique WHERE id_musique = @id_musique";
+            cmd.Parameters.AddWithValue("@id_musique", id_musique);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                musique = new Musique(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetTimeSpan(2),
+                    reader.GetString(3),
+                    reader.GetString(4)
+                );
+            }
+            reader.Close();
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Erreur SQL : {ex.Message}");
+        }
+        finally
+        {
+            close();
+        }
+        return musique;
     }
 
-    public static void GetOneByName(string titre_musique)
+    public static Musique GetOneByName(string titre_musique)
     {
         open();
-        SqlCommand cmd = new SqlCommand();
-        cmd.Connection = connexion;
-        cmd.CommandText = "SELECT * FROM Musique WHERE titre_musique = @titre_musique";
-        cmd.Parameters.AddWithValue("@titre_musique", titre_musique);
-        cmd.ExecuteNonQuery();
-        close();
+        Musique musique = null;
+        try
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connexion;
+            cmd.CommandText = "SELECT * FROM Musique WHERE titre_musique = @titre_musique";
+            cmd.Parameters.AddWithValue("@titre_musique", titre_musique);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                musique = new Musique(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetTimeSpan(2),
+                    reader.GetString(3),
+                    reader.GetString(4)
+                );
+            }
+            reader.Close();
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Erreur SQL : {ex.Message}");
+        }
+        finally
+        {
+            close();
+        }
+        return musique;
     }
 
     public static void DeleteMusique(int id_musique)
     {
         open();
-        SqlCommand cmd = new SqlCommand();
-        cmd.Connection = connexion;
-        cmd.CommandText = "DELETE FROM Musique WHERE id_musique = @id_musique";
-        cmd.Parameters.AddWithValue("@id_musique", id_musique);
-        cmd.ExecuteNonQuery();
-        close();
+        try
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connexion;
+            cmd.CommandText = "DELETE FROM Musique WHERE id_musique = @id_musique";
+            cmd.Parameters.AddWithValue("@id_musique", id_musique);
+            cmd.ExecuteNonQuery();
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine($"Erreur SQL : {ex.Message}");
+        }
+        finally
+        {
+            close();
+        }
     }
 }
